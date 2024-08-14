a91396f42ae068a6fddb0c6bf49aa01eeab5525d
f8devv <faith@aintboard.com>
2024-08-15 05:36:56 +0800

    Add Spinner to LoginProcess loading

--- src/pages/LoginProcess.tsx
+++ src/pages/LoginProcess.tsx
@@ -1,62 +1,42 @@
+/* eslint-disable max-len */
 import { useEffect } from 'react';
-
 import { useLocation, useNavigate } from 'react-router-dom';
-
-import useFetchUserData from '../hooks/useFetchUserData';
+import useLoginUser from '../hooks/useFetchUserData';
 import { useAuth } from '../context/AuthContext';
 import { Endpoints } from '../constants/Endpoints';
-
-/**
- * Temporary login process page shown as user is logging in.
- */
+// import Spinner from '../components/Spinner/Spinner';
+// import Spinner2 from '../components/Spinner2/Spinner2';
+// import Spinner3 from '../components/Spinner3/Spinner3';
+import Spinner3 from '../components/Spinner4/Spinner4';
 const LoginProcessPage = () => {
-	// context for handling user data
 	const { setUserData, setIsLoggedIn } = useAuth();
-
-	// handles navigation
 	const navigate = useNavigate();
-
-	// used in retrieving query params
 	const location = useLocation();
-
-	// retrieve provider and uuid from query params to be used for getting user data
 	const queryParams = new URLSearchParams(location.search);
-	const uuid = queryParams.get('uuid') as string;
+	const key = queryParams.get('key') as string;
 	const provider = queryParams.get('provider') as string;
-
-	// fetch user data
-	const { data, loading, error } = useFetchUserData(Endpoints.fetchUserData, provider, uuid);
-
+	const { data, loading, error } = useLoginUser(Endpoints.loginUser, provider, key);
 	useEffect(() => {
 		if (loading || error) {
 			return;
 		}
-
-		// if data is retrieved successfully, set user data and login state
 		if (data) {
 			setUserData(data);
 			setIsLoggedIn(true);
-
-			// if redirect uri is provided, then redirectu ser
 			const redirectUri = localStorage.getItem('login_redirect_uri');
 			if (redirectUri) {
 				window.location.href = redirectUri;
 			} else {
-				// default to themes page if no redirect uri
 				navigate('/themes');
 			}
 		}
 	}, [loading, error, data]);
-
-	// todo: improve loading display
-	if (loading) {
-		return <div>Loading...</div>;
-	}
-
-	// todo: redirect to error page
-	if (error) {
-		return <div>Error: {error.message}</div>;
-	}
-}
-
-export default LoginProcessPage
+	// Apply black background during loading
+	return (
+		<div className="h-screen w-full bg-black flex justify-center items-center">
+			{loading && <Spinner3 />}
+			{error && <div className="text-white">Error: {error.message}</div>}
+		</div>
+	);
+};
+export default LoginProcessPage;
