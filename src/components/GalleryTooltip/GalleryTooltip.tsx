fd1674ce8aa02d77087ee763d62baed258fb2514
tjtanjin <cjtanjin@gmail.com>
2025-05-30 00:07:14 +0800

    fix: Fix tooltips not showing on mobile

--- src/components/GalleryTooltip/GalleryTooltip.tsx
+++ src/components/GalleryTooltip/GalleryTooltip.tsx
@@ -1,5 +1,6 @@
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
+import { useEffect, useState } from 'react';
 
 /**
  * Shows tooltip for children based on specified content and placement (top, left, right or bottom).
@@ -13,13 +14,54 @@ const GalleryTooltip: React.FC<{
   placement?: 'top' | 'right' | 'bottom' | 'left';
   children: React.ReactElement;
 }> = ({ content, placement = 'right', children }) => {
+  const [open, setOpen] = useState(false);
+  const [isTouchDevice, setIsTouchDevice] = useState(false);
+
+  useEffect(() => {
+    setIsTouchDevice(window.matchMedia('(hover: none)').matches);
+  }, []);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClick = () => {
+    if (isTouchDevice) {
+      setOpen((prevOpen) => !prevOpen);
+    }
+  };
+
+  const handleMouseEnter = () => {
+    if (!isTouchDevice) {
+      handleTooltipOpen();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (!isTouchDevice) {
+      handleTooltipClose();
+    }
+  };
+
   return (
     <Tooltip
       title={<Typography style={{ fontSize: '0.9rem', lineHeight: 1.5 }}>{content}</Typography>}
       placement={placement}
       arrow
+      open={open}
+      onClose={handleTooltipClose}
+      onOpen={handleTooltipOpen}
+      disableFocusListener
+      disableHoverListener={!isTouchDevice}
+      disableTouchListener={isTouchDevice}
     >
-      {children}
+      <span onClick={handleClick} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        {children}
+      </span>
     </Tooltip>
   );
 };
