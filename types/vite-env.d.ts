a764a253247fae3c1ad4a04164ab3e77f38856db
tjtanjin <cjtanjin@gmail.com>
2025-06-01 23:49:08 +0800

    chore: Minor cleanup

--- types/vite-env.d.ts
+++ types/vite-env.d.ts
@@ -3,8 +3,6 @@
 type ImportMetaEnv = {
   readonly VITE_GITHUB_APP_CLIENT_ID: string;
   readonly VITE_GALLERY_API_URL: string;
-  readonly VITE_GITHUB_THEMES_CACHE_URL: string;
-  readonly VITE_GITHUB_LOGIN_URL: string;
 };
 
 type ImportMeta = {
