dd1feb808cd1d59bdb1ac607ccbb28102a0bd266
tjtanjin <cjtanjin@gmail.com>
2025-06-03 21:32:28 +0800

    chore: Minor cleanup

--- src/styleMock.js
+++
@@ -1,3 +0,0 @@
-// src/styleMock.js
-// Mocks CSS imports for Vitest
-export default {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,16 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-// @ts-ignore
-import { defineConfig as defineVitestConfig } from 'vitest/config';
+import { loadEnv } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
 
 export default ({ mode }) => {
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
 	return defineConfig({
-		resolve: {
-			alias: [
-				// Keeping only the general CSS alias
-				{
-					find: /^.+\.(css|less|scss|sass)$/,
-					replacement: path.resolve(__dirname, 'src/styleMock.js'),
-				},
-			],
-		},
 		assetsInclude: ['**/*.svg', '**/*.png', '**/*.wav'],
 		build: {
 			outDir: path.resolve(__dirname, 'dist'),
@@ -52,13 +41,13 @@ export default ({ mode }) => {
 			host: true,
 			port: 3000,
 		},
-		test: defineVitestConfig({
+		test: {
 			globals: true,
 			environment: 'jsdom',
 			setupFiles: './src/setupTests.js',
 			deps: {
 				inline: ['react-chatbotify'],
 			},
-		}),
+		},
 	});
 };
