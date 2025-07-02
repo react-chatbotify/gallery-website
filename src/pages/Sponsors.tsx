5b88df87b0b36b5a2fca563e84c1100f66224947
Mashrukh Islam <mushroomgenie@Iceberg.local>
2025-07-02 16:22:01 +0530

    fix click issue

--- src/pages/Sponsors.tsx
+++ src/pages/Sponsors.tsx
@@ -1,9 +1,8 @@
-import { Box, Skeleton, Tabs, Typography } from '@mui/material';
+import { Box, Skeleton, Tab, Tabs, Typography } from '@mui/material';
 import React, { lazy, Suspense, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const Footer = lazy(() => import('@/components/Home/Footer'));
-import SponsorTab from '@/components/Sponsors/SponsorTab';
 
 const Sponsors: React.FC = () => {
   const [currentSponsorCategory, setCurrentSponsoryCategory] = useState(0);
@@ -27,9 +26,45 @@ const Sponsors: React.FC = () => {
           value={currentSponsorCategory}
           onChange={handleCategoryChange}
         >
-          <SponsorTab currentSponsorCategory={currentSponsorCategory} textValue="categories.monthly" value={0} />
-          <SponsorTab currentSponsorCategory={currentSponsorCategory} textValue="categories.one_time" value={1} />
-          <SponsorTab currentSponsorCategory={currentSponsorCategory} textValue="categories.existing" value={2} />
+          <Tab
+            label={t('categories.monthly')}
+            disableRipple
+            sx={{
+              textTransform: 'none',
+              fontWeight: 'bold',
+              backgroundColor: `${currentSponsorCategory === 0 ? '#3B3B3B80' : null}`,
+              my: `4px`,
+              mx: '4px',
+              border: '1px none #27272A',
+              borderRadius: '8px',
+            }}
+          />
+          <Tab
+            label={t('categories.one_time')}
+            disableRipple
+            sx={{
+              textTransform: 'none',
+              fontWeight: 'bold',
+              backgroundColor: `${currentSponsorCategory === 1 ? '#3B3B3B80' : null}`,
+              my: `4px`,
+              mx: '4px',
+              border: '1px none #27272A',
+              borderRadius: '8px',
+            }}
+          />
+          <Tab
+            label={t('categories.existing')}
+            disableRipple
+            sx={{
+              textTransform: 'none',
+              fontWeight: 'bold',
+              backgroundColor: `${currentSponsorCategory === 2 ? '#3B3B3B80' : null}`,
+              my: `4px`,
+              mx: '4px',
+              border: '1px none #27272A',
+              borderRadius: '8px',
+            }}
+          />
         </Tabs>
       </Box>
       <Suspense fallback={<Skeleton variant="rectangular" height={200} />}>
